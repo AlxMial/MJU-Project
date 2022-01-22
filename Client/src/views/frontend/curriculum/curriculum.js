611c840e40f5eccad5d67f22538386a37e9f5465
PongpatWannarat <pongpat-w@hotmail.com>
2022-01-22 17:32:14 +0700

    update subject

--- Client/src/views/frontend/curriculum/curriculum.js
+++ Client/src/views/frontend/curriculum/curriculum.js
@@ -1,7 +1,7 @@
 import TimeAgo from 'timeago-react';
 import React from 'react'
-import { useHistory, Link,useParams } from "react-router-dom";
-import { useState, useEffect,useContext } from 'react';
+import { useHistory, Link, useParams } from "react-router-dom";
+import { useState, useEffect, useContext } from 'react';
 import { AuthContext } from '../../../services/AuthContext';
 import axios from "axios";
 import FilesServices from '../../../services/files';
@@ -13,7 +13,7 @@ export default function Curriculum() {
     const [searchText, setSearchText] = useState("");
     const [tagText, setTagText] = useState('');
     const [valueSearch, setValueSearch] = useState('');
-    const [learning,setLearning] = useState('');
+    const [learning, setLearning] = useState('');
     const history = useHistory();
     const { authState } = useContext(AuthContext);
     const [optionsLearning, setOptionsLearning] = useState([]);
@@ -27,31 +27,31 @@ export default function Curriculum() {
         setValueSearch('');
     }
 
-    const ChangeLearning = () =>{
+    const ChangeLearning = () => {
         const learningId = localStorage.getItem('learningPathId');
-        if(optionsLearning.length > 0)
-            return  optionsLearning.filter(x => x.value === learningId.toString())[0].label;
+        if (optionsLearning.length > 0)
+            return optionsLearning.filter(x => x.value === learningId.toString())[0].label;
     }
 
     async function fetchLearning() {
-        const response = await axios(urlPath+"/learning");
+        const response = await axios(urlPath + "/learning");
         const body = response.data.listLearning;
         var JsonLearning = [];
-        body.forEach(field => JsonLearning.push({value: field.id.toString(),label: field.LearningPathNameTH }))
+        body.forEach(field => JsonLearning.push({ value: field.id.toString(), label: field.LearningPathNameTH }))
         setOptionsLearning(JsonLearning)
     }
 
     function fetchCourse() {
         const learningPathId = localStorage.getItem('learningPathId');
-        const data = {learningPathId:learningPathId, Type:id};
-        axios.post(urlPath+"/courses/getCourseByTypeAndLearning",data,{
-            headers: {accessToken : localStorage.getItem("accessToken")}
-        }).then((response)=>{
-            for(const value of response.data) {
-                 var JsonTags = JSON.parse(value.CurriculumTag);
+        const data = { learningPathId: learningPathId, Type: id };
+        axios.post(urlPath + "/courses/getCourseByTypeAndLearning", data, {
+            headers: { accessToken: localStorage.getItem("accessToken") }
+        }).then((response) => {
+            for (const value of response.data) {
+                var JsonTags = JSON.parse(value.CurriculumTag);
                 let StringJson = ([]);
-                for(var i=0;i<JsonTags.length;i++){
-                    StringJson[i] = {id:value.id,name:JsonTags[i].name}
+                for (var i = 0; i < JsonTags.length; i++) {
+                    StringJson[i] = { id: value.id, name: JsonTags[i].name }
                 }
                 setTags(StringJson);
             }
@@ -60,73 +60,72 @@ export default function Curriculum() {
         });
     }
 
-    const ConvertBuffer = (value,id) => {
+    const ConvertBuffer = (value, id) => {
         var show_date = document.getElementById(id);
-        if(show_date !== null)
-        {   
+        if (show_date !== null) {
             show_date.innerHTML = FilesServices.buffer64UTF8(value);
         }
     }
-      
-    useEffect(()=>{
+
+    useEffect(() => {
         fetchLearning();
         fetchCourse();
-    },[]);
+    }, []);
 
     return (
         <>
             <div className="relative pt-20 pb-68 flex max-h-screen-37 bg-darkgreen-mju">
-                <div className="container px-4 relative mx-auto lg:w-10/12 mt-2 flex flex-wrap">
+                <div className="container px-12 relative mx-auto lg:w-10/12 mt-2 flex flex-wrap">
                     <div className="w-full lg:w-3/12">
                         <i className="fas fa-arrow-left text-white text-sm cursor-pointer " onClick={() => history.goBack()}>
                             <span>&nbsp;กลับ</span>
                         </i>
                     </div>
-                        <div className='w-full lg:w-6/12'>
+                    <div className='w-full lg:w-6/12'>
                         <div className="px-4 w-full justify-center frmSearch ">
-                        <div className="w-full ml-auto mr-auto">
-                            <h1 className="text-white mt-4 font-semibold text-2xl">
-                                ค้นหาหลักสูตร
-                            </h1>
-                            <div className='flex mt-4'>
-                                <label className="block uppercase text-white text-sm font-bold mb-2 label-form"> ค้นหา </label >
-                                <input className='text-form border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring ease-linear transition-all duration-150'
-                                    type="text"
-                                    value={searchText}
-                                    onChange={(e) => {setSearchText(e.target.value)}}
-                                />
-                            </div>
-                            <div className='flex mt-2'>
-                                <label className="block uppercase text-white text-sm font-bold mb-2 label-form"> แท็ก </label >
-                                <textarea value={tagText}        rows="4" onChange={e => setTagText(e.target.value)}
-                                    className='text-form border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring ease-linear transition-all duration-150' />
-                            </div>
+                            <div className="w-full ml-auto mr-auto">
+                                <h1 className="text-white mt-4 font-semibold text-2xl">
+                                    ค้นหาหลักสูตร
+                                </h1>
+                                <div className='flex mt-4'>
+                                    <label className="block uppercase text-white text-sm font-bold mb-2 label-form"> ค้นหา </label >
+                                    <input className='text-form border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring ease-linear transition-all duration-150'
+                                        type="text"
+                                        value={searchText}
+                                        onChange={(e) => { setSearchText(e.target.value) }}
+                                    />
+                                </div>
+                                <div className='flex mt-2'>
+                                    <label className="block uppercase text-white text-sm font-bold mb-2 label-form"> แท็ก </label >
+                                    <textarea value={tagText} rows="4" onChange={e => setTagText(e.target.value)}
+                                        className='text-form border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring ease-linear transition-all duration-150' />
+                                </div>
 
-                            <div className='flex flex-wrap mt-2'>
-                                <label className='label-form'></label>
-                                <button onClick={() => clearSearch()}
-                                    className="w-3/12 lg:w-3/12 bg-green-mju text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-3 rounded shadow hover:shadow-md outline-none focus:outline-none mr-2 ease-linear transition-all duration-150"
-                                    type="button">
-                                    <i className="fas fa-redo-alt"></i>&nbsp;ล้างค่า
-                                </button>
-                                <button onClick={() => setValueSearch(searchText)}
-                                    className="w-6/12 lg:w-3/12 bg-green-mju text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-3 rounded shadow hover:shadow-md outline-none focus:outline-none ease-linear transition-all duration-150"
-                                    type="button">
-                                    <i className="fas fa-filter"></i>&nbsp;ใช้ตัวกรอง
-                                </button>
-                            </div>
+                                <div className='flex flex-wrap mt-2'>
+                                    <label className='label-form'></label>
+                                    <button onClick={() => clearSearch()}
+                                        className="w-3/12 lg:w-3/12 bg-green-mju text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-3 rounded shadow hover:shadow-md outline-none focus:outline-none mr-2 ease-linear transition-all duration-150"
+                                        type="button">
+                                        <i className="fas fa-redo-alt"></i>&nbsp;ล้างค่า
+                                    </button>
+                                    <button onClick={() => setValueSearch(searchText)}
+                                        className="w-6/12 lg:w-3/12 bg-green-mju text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-3 rounded shadow hover:shadow-md outline-none focus:outline-none ease-linear transition-all duration-150"
+                                        type="button">
+                                        <i className="fas fa-filter"></i>&nbsp;ใช้ตัวกรอง
+                                    </button>
+                                </div>
 
+                            </div>
                         </div>
-                    </div>
                     </div>
                 </div>
             </div>
-            <div className="container relative mx-auto lg:w-10/12 mt-2 px-4 py-4">
+            <div className="container relative mx-auto lg:w-10/12 mt-2 px-12 py-4">
                 <div className='title text-xl font-bold px-4'>
                     {ChangeLearning()}
                 </div>
             </div>
-            <div className="container relative mx-auto lg:w-10/12 mt-2">
+            <div className="container relative mx-auto lg:w-10/12 mt-2 px-12">
                 <div className="w-full">
                     <div className='flex flex-wrap '>
                         {listCourses.filter((item) => {
@@ -136,27 +135,27 @@ export default function Curriculum() {
                             }
                         }).map((item) =>
                             <Link to={`/home/subject/${item.id}`} key={item.id} className="card px-4 md:w-4/12 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 rounded-lg">
-                                {/* <img
+                                <img
                                     alt="..."
-                                   
+                                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRxgWGxEtMlqQqTJi9INeFmSksx54dZoSLxg&usqp=CAU'
                                     className="w-full align-middle rounded-t-lg"
-                                /> */}
+                                />
                                 <blockquote className="blockquote relative p-4 mb-4 shadow-lg rounded-b-lg max-h-200-px ">
                                     <h4 className="text-xl font-bold ">
                                         {item.CurriculumNameTH}
                                     </h4>
                                     <div className='mt-2 text-editor THSarabunBold text-sm-texteditor'>
-                                        <div dangerouslySetInnerHTML={{__html: FilesServices.buffer64UTF8(item.DescriptionTH)}}></div>
+                                        <div dangerouslySetInnerHTML={{ __html: FilesServices.buffer64UTF8(item.DescriptionTH) }}></div>
                                     </div>
                                     <footer>
                                         <div>
                                             <TimeAgo datetime={item.createdAt} />
                                         </div>
                                         <div className='mt-1'>
-                                            { 
-                                                tags.filter((tagsitem) => tagsitem.id === item.id).map(function(value){
-                                                    return ( <label className='tag py-1 mt-4 px-2 text-xs text-blue-mju-home mr-2'>{value.name}</label>)
-                                            })}
+                                            {
+                                                tags.filter((tagsitem) => tagsitem.id === item.id).map(function (value) {
+                                                    return (<label key={value.id} className='tag py-1 mt-4 px-2 text-xs text-blue-mju-home mr-2'>{value.name}</label>)
+                                                })}
                                         </div>
                                     </footer>
 
--- Client/src/views/frontend/subject/subject.js
+++ Client/src/views/frontend/subject/subject.js
@@ -1,12 +1,57 @@
-import React from 'react'
-import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { useHistory, Link } from "react-router-dom";
 import CommentBox from './CommentBox';
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 import './subject.css'
 
 export default function Subject() {
-
+    let { id } = useParams();
+    const [courseData, setCourseData] = useState([]);
+    const [subjectData, setSubjectData] = useState([]);
     const history = useHistory();
+
+    async function fetchCourse() {
+        let response = await axios(
+            `http://localhost:3001/courses/byId/${id}`
+        );
+        let data = await response.data;
+        if (data !== null) {
+            //   for(var columns in response.data) {
+            //     if(columns === "DescriptionTH" || columns === "DescriptionENG")
+            //     formik.setFieldValue(columns,FilesService.buffer64UTF8(response.data[columns]), false)
+            //     else formik.setFieldValue(columns, response.data[columns], false)
+            //   }
+            setCourseData(response.data);
+            console.log(courseData);
+        }
+        else { console.log('No Data') }
+    }
+
+    async function fetchSubject() {
+        let response = await axios(`http://localhost:3001/subjects/byCoursesId/${id}`);
+        let subjects = await response.data;
+        if (subjects !== null) {
+            //   for(var columns in response.data) {
+            //     if(columns === "ContentTH" || columns === "ContentENG")
+            //     {  
+            //       formikSubject.setFieldValue(columns,FilesService.buffer64UTF8(response.data[columns]), false) 
+            //     }
+            //     else 
+            //       formikSubject.setFieldValue(columns, response.data[columns], false)
+            //   }
+            setSubjectData(response.data);
+            console.log(subjectData);
+            // setIsNewSubject(true);
+            // setIsEnableSubjectControl(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchCourse();
+        fetchSubject();
+    }, []);
 
     const data = {
         post: {
@@ -43,9 +88,48 @@ export default function Subject() {
         ]
     };
 
+    const subjectDataTemp = [
+        {
+            id: 0,
+            SubjectNameTH: "ชื่อ Object TH",
+            SubjectNameEng: "Object Name Eng",
+            SubjectOfHour: "40"
+        },
+        {
+            id: 1,
+            SubjectNameTH: "ความรู้พื้นฐาน ถิ่นกำเนิด",
+            SubjectNameEng: "kwamroo phuenthan thinkamnerd",
+            SubjectOfHour: "120"
+        },
+        {
+            id: 2,
+            SubjectNameTH: "นายกฯ ไม่ยอมลาออกสักที แปดปีแล้วไอ่สัส",
+            SubjectNameEng: "i here too",
+            SubjectOfHour: "8"
+        },
+        {
+            id: 3,
+            SubjectNameTH: "นายกฯ ไม่ยอมลาออกสักที แปดปีแล้วไอ่สัส 2",
+            SubjectNameEng: "i here too 2",
+            SubjectOfHour: "80"
+        },
+        {
+            id: 4,
+            SubjectNameTH: "บางคนก็ไม่ยอมกลับบ้าน อยู่แต่เยมันดี",
+            SubjectNameEng: "Leader of villege no. 10",
+            SubjectOfHour: "55"
+        },
+        {
+            id: 5,
+            SubjectNameTH: "บางคนก็ไม่ยอมกลับบ้าน อยู่แต่เยมันดี 2",
+            SubjectNameEng: "Leader of villege no. 10 2",
+            SubjectOfHour: "90"
+        }
+    ];
+
     return (
         <>
-            <div className="container pt-20 relative mx-auto lg:w-10/12 flex flex-wrap">
+            <div className="container pt-20 px-12 relative mx-auto lg:w-10/12 flex flex-wrap">
                 <div className='mx-auto w-full header-bar'>
                     <div className="w-full lg:w-3/12">
                         <i className="fas fa-arrow-left text-sm cursor-pointer " onClick={() => history.goBack()}>
@@ -53,9 +137,32 @@ export default function Subject() {
                         </i>
                     </div>
                 </div>
+                <div className='mt-3 mb-3 text-sm'> หน่วยที่ 1.2 ข้าวเพื่อชีวิตและสังคม</div>
                 <div className='w-full mt-3'>
                     <div className="min-h-screen-35 px-4 relative flex flex-col min-h-3 break-words bg-white w-full mb-6 rounded-lg shadow-lg">
+                        <div className='img-course lg:w-4/12 text-center flex flex-wrap mx-auto'>
+                            <img
+                                className="w-full align-middle"
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRxgWGxEtMlqQqTJi9INeFmSksx54dZoSLxg&usqp=CAU" alt="" />
+                        </div>
+                        <div className='course-content lg:w-8/12 leading-none mx-auto mt-3 mb-3'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque repudiandae quaerat maiores quo, eligendi repellendus officia aliquam fugit numquam fuga laudantium enim eveniet quis reprehenderit nisi quae facere quasi culpa! Nostrum eligendi blanditiis rem vero cum esse quia magni sunt unde beatae numquam quod officiis, dicta ipsam maxime non placeat.
+                        </div>
 
+                        <div className='subject-content px-4 py-1 rounded-lg lg:w-8/12 mx-auto mt-3 mb-3'>
+                            {
+                                subjectDataTemp.map((item) => {
+                                    return (
+                                        <Link to='#' key={item.id}>
+                                            <div className='font-bold subjectName'>{item.SubjectNameTH}</div>
+                                            <div className='text-mute subjectOfHour'>
+                                                {item.SubjectOfHour} นาที
+                                            </div>
+                                        </Link>
+                                    )
+                                })
+                            }
+                        </div>
                     </div>
                 </div>
 
