78050f639174824bc3dff65cbd3d43c62fd517a7
AlxMial <40629098+AlxMial@users.noreply.github.com>
2022-01-23 18:56:37 +0700

    Update index.js

--- Server/models/index.js
+++ Server/models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 
 /* Connect Database With Mysql Server */
-const env = process.env.NODE_ENV || 'development';
+const env = 'development' || 'development';
 const config = require(__dirname + '../../config/config.json')[env];
 const db = {};
 
