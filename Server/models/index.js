377419c5e3367f39942b5543d56b98f301a52cd2
AlxMial <40629098+AlxMial@users.noreply.github.com>
2022-01-23 18:51:28 +0700

    Update index.js

--- Server/models/index.js
+++ Server/models/index.js
@@ -7,7 +7,7 @@ const basename = path.basename(__filename);
 
 /* Connect Database With Mysql Server */
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '../../config/config.json')[env];
 const db = {};
 
 
